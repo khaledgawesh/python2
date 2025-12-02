295608b71ed2007e433cb2a57700dee218faed07
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-02 20:20:34 +0200

    last

--- oop/file_enc.py
+++ oop/file_enc.py
@@ -1,45 +1,21 @@
-# class Student:
-#   def __init__(self, name, age):
-#     self.__name = name
-#     self.age = age
-
-#   def get_name(self):
-#     return self.__name
-  
-#   def set_name(self, name):
-#     self.__name = name
-
-
-# std1 = Student("khaled", 20)
-# std_name = std1.get_name()
-# print(std_name)              
-
-# std1.set_name("khaled gawesh")
-# print(std1.get_name())
-
-
-##############################################
-
 class Student:
-  def __init__(self, name, age):
-    self.__name = name
-    self.age = age
-
-  def get_name(self):
-      return self.__name
-  
-  def set_name(self, name):
-      self.__name = name
-
-  def get_name(self):
-      return self.__age
-  
-  def set_name(self, name):
-      self.__age = age
-
-  def get_all_info(self):
-     return self.__name, self.__age
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
     
-std1 = Student("ahmed", 55)
-print(std1.age)
-print(list)
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_all_info(self):
+        return [self.__name, self.__age]
+std1 = Student('ahmed', 20)
+print(std1.get_all_info())
