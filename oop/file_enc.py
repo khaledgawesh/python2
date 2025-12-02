f67f35edde55655b7f3735a32791df3503a6170b
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-02 18:53:59 +0200

    0001

--- oop/file_enc.py
+++ oop/file_enc.py
@@ -5,8 +5,13 @@ class Student:
 
   def get_name(self):
     return self.__name
+  
+  def set_name(self, name):
+    self.__name = name
 
 
-std1 = Student("khaled gawesh", 20)
+std1 = Student("khaled", 20)
 std_name = std1.get_name()
+print(std_name)
+std1.set_name("khaled gawesh")
 print(std_name)
