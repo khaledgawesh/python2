a46d059eeb210d8ab8a8d39af96333ff729a0100
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-02 18:40:54 +0200

    now

--- oop/file_enc.py
+++ oop/file_enc.py
@@ -1,5 +1,12 @@
-class Student():
+class Student:
   def __init__(self, name, age):
-    self.name = name
+    self.__name = name
     self.age = age
-    
+
+  def get_name(self):
+    return self.__name
+
+
+std1 = Student("khaled gawesh", 20)
+std_name = std1.get_name()
+print(std_name)
